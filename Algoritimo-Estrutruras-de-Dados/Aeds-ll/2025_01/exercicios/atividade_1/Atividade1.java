68d03b5100f44cfb43c1e419f76177de0471bd5c
Felipe Mizher <130677681+FelipeMizher@users.noreply.github.com>
2025-12-19 19:16:46 -0300

    Add files via upload

---
+++ Algoritimo-Estrutruras-de-Dados/Aeds-ll/2025_01/exercicios/atividade_1/Atividade1.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//Atuvudade que complementa o Exemplo1.java e Exemplo2.java
+//Usar junto com o pub_tipo3.in
+
+class Atividade1{
+  public static void ProcessarRegistro(int id, double preco, String tempo, String cidade){
+    System.out.printf("Registro lido: %d %.2f %s %s\n", id, preco, tempo, cidade);
+  }
+
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int id;
+    double preco;
+    String tempo;
+    String cidade;
+
+    while((id = scanner.nextInt()) != 0){
+      preco = scanner.nextDouble();
+      tempo = scanner.next();
+      cidade = scanner.nextLine().trim();
+      ProcessarRegistro(id, preco, tempo, cidade);
+    }
+
+    scanner.close();
+  }
+}
